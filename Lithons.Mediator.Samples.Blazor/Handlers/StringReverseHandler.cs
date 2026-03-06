8b3048ade4735e7e96cbe44c4948f2605a59fb42
Yop Spanjers <ysp@quadira.com>
2026-03-06 16:45:00 +0100

    Add sample project for debugging

---
+++ Lithons.Mediator.Samples.Blazor/Handlers/StringReverseHandler.cs
@@ -0,0 +1,13 @@
+﻿using Lithons.Mediator.Abstractions.Contracts;
+
+namespace Lithons.Mediator.Samples.Blazor.Handlers;
+
+public record StringReverseRequest(string Value) : IRequest<string>;
+public class StringReverseHandler : IRequestHandler<StringReverseRequest, string>
+{
+    public Task<string> HandleAsync(StringReverseRequest request, CancellationToken cancellationToken)
+    {
+        var reversed = new string(request.Value.Reverse().ToArray());
+        return Task.FromResult(reversed);
+    }
+}
